fc2f91f58ad6e8d6ac1cc21a9388af36b1800293
Lorenzo Scarpa <scarpal@AULE.IT>
2017-10-11 17:21:29 +0200

    first commit

---
+++ MvcMovie/App_Start/RouteConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcMovie
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+             routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{name}",
+                defaults: new { controller = "Home", action = "Index", name = UrlParameter.Optional }
+            );
+
+            //routes.MapRoute(
+            //    name: "Default2",
+            //    url: "{controller}/{action}/{id}/{numTimes}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional, numTimes = UrlParameter.Optional }
+            //);
+
+            routes.MapRoute(
+                name: "Root2",
+                url: "{controller}/{action}/{name}/{numTimes}",
+                defaults: new { controller = "Home", action = "Index", name = UrlParameter.Optional, numTimes = UrlParameter.Optional }
+            );
+
+             
+        }
+    }
+}
---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            int a = 35*4+2;
+            ViewBag.Number=a;
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+
+        
+        //public string Welcome2(int? id, int numTimes = 1)
+        //{
+        //     return HttpUtility.HtmlEncode("Hello " + id + ", NumTimes is: " + numTimes);
+        //}
+    }
+}
